83768184ac6fdf08d68e8c03fd96f5f0726ccab1
Dejai Huffmon <dejaihuffmon@gmail.com>
2023-04-23 20:31:49 -0400

    No changes were made

--- About Me/ContentView.swift
+++ About Me/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemGray3)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 25) {
+                Image("Dejai")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                HStack(spacing: 20) {
+                    Text("Dejai Monet")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.black)
+                    Text("Host of I Think Therefore I'm Black")
+                }
+               
+                
+                Text("My name is Dejai Monet Huffmon. I was born in Atlanta, Georgia on January 14th. 1998. I was raised in Chicago, IL where I spent most of my life up until 2016 when I moved to St. Louis for college. After college I relocated back to Atlanta, Georgia to pursue my passion in Film and Entertainment. I currently work as a middle school Special Educaiton Teacher and am a soon to be Teach for America Alumni!")
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .shadow(radius: 50)
+            .padding()
         }
-        .padding()
+       
+
     }
 }
 
